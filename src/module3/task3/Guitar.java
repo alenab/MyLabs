7c49b227eed76ca710ab75613ac169c24a7d3cf5
Olga Kulikova <masyakot@gmail.com>
2015-12-18 23:00:21 +0200

    added comments

--- src/module3/task3/Guitar.java
+++ src/module3/task3/Guitar.java
@@ -1,5 +1,5 @@
 package module3.task3;
 
-
+//лишние строки-пробелы тоже убираем. между package и class - одна пустая строка
 public class Guitar extends MusicalInstrument {
 }
--- src/module3/task3/MusicShop.java
+++ src/module3/task3/MusicShop.java
@@ -1,5 +1,5 @@
 package module3.task3;
 
 public class MusicShop {
-    private MusicalInstrument[] musicalInstrument;
-}
+    private MusicalInstrument[] musicalInstrument; //в наименовании массива больше принято писать во множественном числе
+} // проверить это во всех массивах
