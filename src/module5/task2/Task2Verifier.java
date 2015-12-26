84cf666f12e2392e0edf00af72ee69b0e504a1d1
Olga Kulikova <masyakot@gmail.com>
2015-12-26 23:34:21 +0200

    added comments

--- src/module5/task2/Task2Verifier.java
+++ src/module5/task2/Task2Verifier.java
@@ -5,7 +5,8 @@ public class Task2Verifier {
         int testArray[] = new int[]{3, 2, -1, 5, 10, -5};
         SortSelection.getSort(testArray);
         for (int i = 0; i < testArray.length; i++) {
-            int item = testArray[i];
+            int item = testArray[i]; //временная переменная item излишняя. А еще идея подсказывает даже, что лучше использовать
+            //цикл for-each
             System.out.println(item);
         }
     }
