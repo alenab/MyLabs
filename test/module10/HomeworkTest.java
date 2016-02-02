1a066124284e1e59eaa205fc08e5c54257575dde
Elena Kuzych <alena.barannik@gmail.com>
2016-02-03 00:13:33 +0200

    Модуль 11 (корректировка теста для Модуля10)

--- test/module10/HomeworkTest.java
+++ test/module10/HomeworkTest.java
@@ -1,12 +1,8 @@
 package module10;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 
@@ -15,29 +11,33 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 public class HomeworkTest {
 
-    private static final String FILE_NAME = "secret.txt";
+    private static final File FILE = new File("secret.txt");
     private static final String TEXT = "you should not see this text";
     private static final int DEFAULT_TIMEOUT_MILLIS = 3_000;
 
-    @Test(timeout = DEFAULT_TIMEOUT_MILLIS)
-    public void safeSecureFileTest() throws IOException {
-        Homework.saveSecureFile(FILE_NAME, TEXT);
-        File file = new File(FILE_NAME);
-
-        Assert.assertTrue(file.exists());
-        Assert.assertNotEquals(TEXT, Files.lines(file.toPath()).collect(joining()));
+    @BeforeClass
+    public static void prepareFile() throws IOException {
+        Homework.saveSecureFile(FILE.getName(), TEXT);
     }
 
     @Test(timeout = DEFAULT_TIMEOUT_MILLIS)
-    public void loadSecureFile() throws FileNotFoundException {
-        final String textFromFile = Homework.loadSecureFile(FILE_NAME);
+    public void safeSecureFileTest() throws IOException {
+        Homework.saveSecureFile(FILE.getName(), TEXT);
+
+        Assert.assertTrue(FILE.exists());
+        Assert.assertNotEquals(TEXT, Files.lines(FILE.toPath()).collect(joining()));
+    }
+
+    @Test(timeout = DEFAULT_TIMEOUT_MILLIS)
+    public void loadSecureFile() throws IOException {
+        Homework.saveSecureFile(FILE.getName(), TEXT);
+        final String textFromFile = Homework.loadSecureFile(FILE.getName());
         Assert.assertThat("Content differs from original", textFromFile, equalTo(TEXT));
     }
 
     @AfterClass
     public static void deleteFile() {
-        File file = new File(FILE_NAME);
-        file.delete();
+        FILE.delete();
     }
 }
 
